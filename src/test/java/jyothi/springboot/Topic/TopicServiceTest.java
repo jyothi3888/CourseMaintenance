109f754989ec96b4f81602d94a14d50ccfc3ed98
Sree <sreeyashu@Yashwanths-MacBook-Pro.local>
2021-06-14 11:56:53 +0200

    featurbranch commit

---
+++ src/test/java/jyothi/springboot/Topic/TopicServiceTest.java
@@ -0,0 +1,13 @@
+package jyothi.springboot.Topic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TopicServiceTest {
+
+	@Test
+	void test() {assertTrue(true);
+	}
+
+}
